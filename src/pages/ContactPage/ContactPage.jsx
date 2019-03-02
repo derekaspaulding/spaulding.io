e08b2e8b537da931639c773063e42ce43474cafd
Derek Spaulding <derek@derekaspaulding.com>
2019-03-02 17:09:21 -0600

    style contact form and get state working with formik

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,5 +1,76 @@
 import React from 'react';
-import styles from './contact.module.css';
+import styles from './contact.module.less';
 import MainLayout from '../../components/layouts/MainLayout'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import * as classnames from 'classnames';
 
-export default () => <MainLayout><div className={styles.homeWrapper}>Contact</div></MainLayout>
+function getFieldClasses(hasError) {
+  const errorClass = hasError ? styles.hasError : null;
+  return classnames(styles.field, errorClass);
+}
+
+export default () => (
+  <MainLayout>
+    <div className={styles.wrapper}>
+      <p>
+        You can contact me either by email at 
+        {' '}<a href="mailto:derek@spaulding.io" className={styles.mailLink}>derek@spaulding.io</a>{' '}
+        or by filling out the form below. I will reply as soon as possible.
+      </p>
+
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          message: '',
+        }}
+        validationSchema={
+          yup.object().shape({
+            name: yup.string().required('Required.'),
+            email: yup.string().email('Must be a valid email.').required('Required.'),
+            message: yup.string().required('Required.'),
+          })
+        }
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+        validateOnChange={true}
+      >
+        {({ isSubmitting, errors, touched }) => (
+        <Form>
+          <div className={styles.formItem}>
+            <label htmlFor="name" className={styles.label}>Name</label>
+            <ErrorMessage name="name" component="div" className={styles.error}/>
+            <Field name="name" id="name" className={getFieldClasses(touched.name && errors.name)}/>
+          </div>
+          <div className={styles.formItem}>
+            <label htmlFor="email" className={styles.label}>Email</label>
+            <ErrorMessage name="email" component="div" className={styles.error}/>
+            <Field type="email" name="email" id="email" className={getFieldClasses(touched.email && errors.email)}/>
+          </div>
+          <div className={styles.formItem}>
+            <label htmlFor="message" className={styles.label}>Message</label>
+            <ErrorMessage name="message" component="div" className={styles.error}/>
+            <Field name="message" id="message" component="textarea" className={getFieldClasses(touched.message && errors.message)} rows={4} onInput={resizeTextarea}/>
+          </div>
+          <button type="submit" disabled={isSubmitting} className={styles.submit}>
+            Submit
+          </button>
+        </Form>
+      )}
+      </Formik>
+    </div>
+  </MainLayout>
+)
+
+const resizeTextarea = (event) => {
+  const element = event.currentTarget;
+  console.log(element.scrollHeight)
+  element.style.height = 'auto';
+  element.style.height = `${element.scrollHeight}px`;
+  console.log(element.style)
+}
