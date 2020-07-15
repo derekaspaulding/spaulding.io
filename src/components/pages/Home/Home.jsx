16890aae0f32961eda28fdf38d5005ec17829366
Derek Spaulding <derek@derekaspaulding.com>
2020-07-15 06:05:19 -0500

    update home page with current job (#1)

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -9,9 +9,10 @@ export default () => (
     <img className={styles.headshot} src={headshot} alt="" />
     <article className={styles.about}>
       <p>
-        I am a software engineer at Code42 in Minneapolis, Minnesota. Day to day
-        I work with a great team to build tools to help keep our customer's data
-        safe. I earned a Bachelor's Degree in Computer Science with a minor in
+        I am a software engineer at Target in Minneapolis, Minnesota. Day to
+        day I work to provide a useful and accessible UI experience for tools
+        that help my fellow engineers accross Target be more efficient in their
+        jobs. I earned a Bachelor's Degree in Computer Science with a minor in
         Mathematics from the University of Minnesota in 2017. I have been
         developing software professionally since 2016.
       </p>
